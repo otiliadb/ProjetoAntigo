85f34ed51f7b2347b40eca52c0fa85655be1d4f7
otiliadb <otilia.barbosa@unoesc.edu.br>
2023-03-30 16:56:51 -0300

    segundo comentário

--- src/br/edu/unoesc/HelloGit.java
+++ src/br/edu/unoesc/HelloGit.java
@@ -4,6 +4,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world, Git! ");
+		System.out.println("Hello world, Git, again! ");
+		System.out.println("Hello world, Git, ana again! ");
 
 	}
 
